a663fc851f5637939bf434e805fd1210e159e5f1
William-Meyer <william.meyer0529@gmail.com>
2022-12-07 20:11:19 -0800

    ef

---
+++ Site/scripts/2022_sidebar.js
@@ -0,0 +1,41 @@
+
+(function($) {
+  $(function() {
+    $('.side-dropdown').click(function(){
+      if($(this).find($('.side-dropdown-list')).is(':visible')){
+        $(this).parent().find('.sidebarArrow').css({
+            transform: 'rotate(' + 0 + 'deg)'
+        });
+      }
+      else{
+        $('.sidebarArrow').css({
+            transform: 'rotate(' + 0 + 'deg)'
+        });
+        $(this).parent().find('.sidebarArrow').css({
+            transform: 'rotate(' + 180 + 'deg)'
+        });
+      }
+      $(this).children($('.side-drop-content')).children().addClass('side-active');
+      $(".side-dropdown-list:not(.side-active)").slideUp();
+      $(this).children($('.side-drop-content')).children().removeClass('side-active');
+      $(this).find($('.side-dropdown-list')).slideToggle();//problem
+    });
+    $('#bar-toggle').click(function() {
+      if($(window).width()<983){
+      $('#barList').slideToggle();}
+    });
+    $('#bar-toggle').on('click', function() {
+      this.classList.toggle('active-sidebar');
+    });
+  });
+})(jQuery);
+$( window ).resize(function() {
+  let w = $(window).width();
+  if(w>982){
+    $('#barList').slideDown('fast');
+  }
+  else {
+    $(".side-dropdown-list").slideUp("fast");
+    $('#barList').slideUp('fast');
+  }
+});
