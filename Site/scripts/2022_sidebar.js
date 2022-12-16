ab64531ec5fe358194faddec5e7b148a69ee3987
William-Meyer <william.meyer0529@gmail.com>
2022-12-16 09:23:55 -0500

    Search bar style

--- Site/scripts/2022_sidebar.js
+++ Site/scripts/2022_sidebar.js
@@ -2,6 +2,9 @@
 (function($) {
   $(function() {
     $('.side-dropdown').click(function(){
+      var target = $(event.target), article;
+      console.log(target);
+      if((target.is('.side-dropdown a'))){
       if($(this).find($('.side-dropdown-list')).is(':visible')){
         $(this).parent().find('.sidebarArrow').css({
             transform: 'rotate(' + 0 + 'deg)'
@@ -15,10 +18,11 @@
             transform: 'rotate(' + 180 + 'deg)'
         });
       }
-      $(this).children($('.side-drop-content')).children().addClass('side-active');
-      $(".side-dropdown-list:not(.side-active)").slideUp();
-      $(this).children($('.side-drop-content')).children().removeClass('side-active');
-      $(this).find($('.side-dropdown-list')).slideToggle();//problem
+        $(this).children($('.side-drop-content')).children().addClass('side-active');
+        $(".side-dropdown-list:not(.side-active)").slideUp();
+        $(this).children($('.side-drop-content')).children().removeClass('side-active');
+        $(this).find($('.side-dropdown-list')).slideToggle();
+      }
     });
     $('#bar-toggle').click(function() {
       if($(window).width()<983){
