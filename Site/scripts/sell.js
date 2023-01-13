e71828b4c1f8ea63c6a6f6bdd3be045ed4c63ccc
William-Meyer <william.meyer0529@gmail.com>
2023-01-13 05:43:07 -0800

    hjgfj

---
+++ Site/scripts/sell.js
@@ -0,0 +1,6 @@
+function sell(){
+  $('.line').css('display','none');
+  $('#message').css('display','none');
+  $('#submit').css('display','none');
+  $('.reveal').css('display','block');
+}
---
+++ Site/scripts/spec.js
@@ -0,0 +1,6 @@
+function submit(){
+  $("input").css('display','none');
+  $("#submit").css('display','none');
+  $('#message').css('display','none');
+  $('.reveal').css('display','block');
+}
